978c26629feabe914569c916052e6688486cd3dc
akorotkov <akorotkov@MacBook-Pro-it2-3.local>
2025-01-15 15:04:00 +0300

    add chain

--- behavioral/chain.php
+++ behavioral/chain.php
@@ -11,6 +11,66 @@ abstract class Handler
     {
         $this->successor = $successor;
     }
+
+    final public function handle(TaskInterface $task): array
+    {
+        $proceed = $this->processing($task);
+        if ($proceed === null && $this->successor) {
+            $proceed = $this->successor->handle($task);
+        }
+
+        return $proceed;
+    }
+
+    abstract public function processing(TaskInterface $task): ?array;
+
+
 }
 
-//TODO доделать паттерн
+interface TaskInterface
+{
+    public function getArray(): array;
+}
+
+
+class DevTask implements TaskInterface
+{
+    private array $arr = [1,2,3];
+
+    public function getArray(): array
+    {
+        return $this->arr;
+    }
+}
+
+class Senior extends Handler
+{
+    public function processing(TaskInterface $task): ?array
+    {
+        return $task->getArray();
+    }
+}
+
+class Middle extends Handler
+{
+    public function processing(TaskInterface $task): ?array
+    {
+        return $task->getArray();
+    }
+}
+
+class Jun extends Handler
+{
+    public function processing(TaskInterface $task): ?array
+    {
+        return null;
+    }
+}
+
+$task = new DevTask();
+
+$senior = new Senior(null);
+$mid = new Middle($senior);
+$jun = new Jun($mid);
+
+var_export($jun->handle($task));
